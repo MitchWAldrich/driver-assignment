7000785cb41d04389e615bedfb76b199aa36e9b8
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-10-18 21:56:22 -0400

    add Board and Order components to render in app

--- rose-rocket-logistics/src/App.js
+++ rose-rocket-logistics/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Board from './components/Board';
+import Order from './components/Order';
 
 function App() {
   return (
     <div className="App">
      <main className="flexbox">
-       Drag and Drop Boards Here
+       <Board id="board-1" className="board">
+         <Order id="order-1" className="order">
+           <p>order one info</p>
+         </Order>
+       </Board>
+
+       <Board id="board-2" className="board">
+         <Order id="order-2" className="order">
+           <p>order two info</p>
+         </Order>
+       </Board>
      </main>
     </div>
   );
