6496d986628c5df34a0e23223125aecd01946d8b
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-10-19 12:29:59 -0400

    map through list of drivers and populate boards using getOrdersByDriverId

--- rose-rocket-logistics/src/App.js
+++ rose-rocket-logistics/src/App.js
@@ -5,12 +5,28 @@ import Board from './components/Board';
 import Order from './components/Order';
 
 import useApplicationData from "./hooks/useApplicationData";
+import { getOrdersByDriverId } from "./helpers/selectors";
 
 function App() {
   const {
     state,
     setDriver
   } = useApplicationData();
+
+  const driverBoards = state.drivers.map( driver => {
+    getOrdersByDriverId(state.orders, driver.id);
+  // const parsedDriverBoards = driverBoard.map(driver => {
+    return (
+      <Board
+        key={driver.id} 
+        id={driver.id}
+        className="board"
+        >
+          <Order id="order-1" className="order">
+          </Order>
+        </Board>
+    )
+  });
 
   return (
     <div className="App">
@@ -20,10 +36,7 @@ function App() {
          </Order>
        </Board>
 
-       <Board id="board-2" className="board">
-         <Order id="order-2" className="order">
-         </Order>
-       </Board>
+       {driverBoards}
      </main>
     </div>
   );
