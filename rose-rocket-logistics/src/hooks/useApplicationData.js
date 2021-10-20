10b8767a85c3288c68d312b2b2055dd22ca2afa5
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-10-19 23:48:42 -0400

    create editOrder and editDriver functions

--- rose-rocket-logistics/src/hooks/useApplicationData.js
+++ rose-rocket-logistics/src/hooks/useApplicationData.js
@@ -20,5 +20,35 @@ export default function useApplicationData() {
     })
   }, []);
 
-  return { state, setDriver }
+  const editOrder = (order, driver_id, description, cost, revenue, isAssigned) => {
+    return (
+      axios.put('/api/orders/:id', {
+        ...order,
+        driver_id,
+        description,
+        cost,
+        revenue,
+        isAssigned
+      }).then(() => {
+        console.log('order updated!')
+      }).catch((e) => {
+        console.log(e.message)
+      })
+    )
+  }
+
+  const editDriver = (order, driverId) => {
+    return (
+      axios.put('/api/orders/:id', {
+        ...order,
+        driver_id: driverId
+      }).then(() => {
+        console.log('driver updated!')
+      }).catch((e) => {
+        console.log(e.message)
+      })
+    )
+  }
+
+  return { state, setDriver, editOrder, editDriver }
 };
