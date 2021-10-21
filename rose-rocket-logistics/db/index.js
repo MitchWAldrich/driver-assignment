978f0a0e9ecaa40725068b29041c2eaeb801afea
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-10-21 00:21:21 -0400

    add database credentials

--- rose-rocket-logistics/db/index.js
+++ rose-rocket-logistics/db/index.js
@@ -3,6 +3,7 @@ const pg = require("pg");
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+  database: 'roserocket',
   user: 'mitch',
   password: 'development'
 });
