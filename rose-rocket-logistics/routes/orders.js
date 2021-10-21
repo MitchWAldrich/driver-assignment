b6421cc28a485be04d457526700ba8782eacca5d
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-10-21 00:24:01 -0400

    remove isAssigned from orders PUT route

--- rose-rocket-logistics/routes/orders.js
+++ rose-rocket-logistics/routes/orders.js
@@ -9,13 +9,14 @@ module.exports = db => {
   });
   
   router.put("/orders/:id", (request, response) => {
-    const { order, driver_id, description, cost, revenue, isAssigned } = request.body;
+    const { id, driver_id, description, cost, revenue } = request.body;
+
     db.query(`
     UPDATE orders
-    SET driver_id = $1, description = $2, cost = $3, revenue = $4, isAssigned = $5
-    WHERE id = $6
-    `, [driver_id, description, cost, revenue, isAssigned, order.id])
-    console.log('updated order:', request.body)
+    SET driver_id = $1, description = $2, cost = $3, revenue = $4
+    WHERE id = $5
+    `, [driver_id, description, cost, revenue, id])
+    // console.log('updated order:', request.body)
     .then((results) => {
       response.status(200)
     })
